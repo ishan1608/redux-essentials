6a9c6fcafd25f92af9294f24c248f494418e8a0d
Ishan <ishan1608@gmail.com>
2025-08-05 17:05:18 +0530

    Using the logged in user instead of asking to pick the user

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { postAdded } from '@/features/posts/postsSlice'
-import { selectAllUsers } from '@/features/users/usersSlice'
+import { selectCurrentUserName } from '@/features/auth/authSlice'
 
 // TS types for the input fields
 // See: https://epicreact.dev/how-to-type-a-react-form-on-submit-handler/
 interface AddPostFormFields extends HTMLFormControlsCollection {
   postTitle: HTMLInputElement
   postContent: HTMLTextAreaElement
-  postAuthor: HTMLSelectElement
 }
 
 interface AddPostFormElements extends HTMLFormElement {
@@ -17,8 +16,7 @@ interface AddPostFormElements extends HTMLFormElement {
 
 export const AddPostForm = () => {
   const dispatch = useAppDispatch()
-
-  const users = useAppSelector(selectAllUsers)
+  const username = useAppSelector(selectCurrentUserName)
 
   const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
     // Prevent server submission
@@ -27,18 +25,11 @@ export const AddPostForm = () => {
     const { elements } = e.currentTarget
     const title = elements.postTitle.value
     const content = elements.postContent.value
-    const authorId = elements.postAuthor.value
 
-    dispatch(postAdded(title, content, authorId))
+    dispatch(postAdded(title, content, username))
 
     e.currentTarget.reset()
   }
-
-  const usersOptions = users.map((user) => (
-    <option key={user.id} value={user.id}>
-      {user.name}
-    </option>
-  ))
 
   return (
     <section>
@@ -46,11 +37,6 @@ export const AddPostForm = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="postTitle">Post Title:</label>
         <input type="text" id="postTitle" defaultValue="" required />
-        <label htmlFor="postAuthor">Author:</label>
-        <select id="postAuthor" name="postAuthor" required>
-          <option value=""></option>
-          {usersOptions}
-        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea id="postContent" name="postContent" defaultValue="" required />
         <button>Save Post</button>
