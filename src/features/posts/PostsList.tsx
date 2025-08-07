3ea1b4defd9b1e8278db4c5d9ede1789e95f8cb5
Ishan <ishan1608@gmail.com>
2025-08-07 19:04:48 +0530

    Fetching posts by dispatching the fetchPosts() async thunk

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,31 +1,50 @@
-import { useAppSelector } from '@/app/hooks'
+import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { Link } from 'react-router-dom'
-import { selectAllPosts } from '@/features/posts/postsSlice'
+import { fetchPosts, selectAllPosts, selectPostsError, selectPostsStatus } from '@/features/posts/postsSlice'
 import { PostAuthor } from '@/features/posts/PostAuthor'
 import { TimeAgo } from '@/features/posts/TimeAgo'
 import { ReactionButtons } from '@/features/posts/ReactionButtons'
+import { useEffect } from 'react'
+import { Spinner } from '@/components/Spinner'
 
 export const PostsList = () => {
   // select the state.posts value from the store into the component
   const posts = useAppSelector(selectAllPosts)
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  const postsStatus = useAppSelector(selectPostsStatus)
+  const postsError = useAppSelector(selectPostsError)
+  const dispatch = useAppDispatch()
 
-  const renderedPosts = orderedPosts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>
-        <Link to={`/posts/${post.id}`}>{post.title}</Link>
-      </h3>
-      <PostAuthor userId={post.user} />
-      <TimeAgo timestamp={post.date} />
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <ReactionButtons post={post} />
-    </article>
-  ))
+  // fetch posts when the component mounts
+  useEffect(() => {
+    if (postsStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postsStatus])
+
+  let content: React.ReactNode = null
+  if (postsStatus === 'pending') {
+    content = <Spinner text="Loading..." />
+  } else if (postsStatus === 'succeeded') {
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    content = orderedPosts.map((post) => (
+      <article className="post-excerpt" key={post.id}>
+        <h3>
+          <Link to={`/posts/${post.id}`}>{post.title}</Link>
+        </h3>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+        <ReactionButtons post={post} />
+      </article>
+    ))
+  } else if (postsStatus === 'failed') {
+    content = <div>{postsError}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
