db69aaf840d1bded5b26b3759ea7913be55b1406
Ishan <ishan1608@gmail.com>
2025-08-11 16:29:55 +0530

    Moving Post display logic to a smaller component

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,11 +1,29 @@
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { Link } from 'react-router-dom'
-import { fetchPosts, selectAllPosts, selectPostsError, selectPostsStatus } from '@/features/posts/postsSlice'
+import { fetchPosts, Post, selectAllPosts, selectPostsError, selectPostsStatus } from '@/features/posts/postsSlice'
 import { PostAuthor } from '@/features/posts/PostAuthor'
 import { TimeAgo } from '@/features/posts/TimeAgo'
 import { ReactionButtons } from '@/features/posts/ReactionButtons'
 import { useEffect } from 'react'
 import { Spinner } from '@/components/Spinner'
+
+interface PostExcerptProps {
+  post: Post
+}
+
+const PostExcerpt = ({ post }: PostExcerptProps) => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <PostAuthor userId={post.user} />
+      <TimeAgo timestamp={post.date} />
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+    </article>
+  )
+}
 
 export const PostsList = () => {
   // select the state.posts value from the store into the component
@@ -26,17 +44,7 @@ export const PostsList = () => {
     content = <Spinner text="Loading..." />
   } else if (postsStatus === 'succeeded') {
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-    content = orderedPosts.map((post) => (
-      <article className="post-excerpt" key={post.id}>
-        <h3>
-          <Link to={`/posts/${post.id}`}>{post.title}</Link>
-        </h3>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-        <ReactionButtons post={post} />
-      </article>
-    ))
+    content = orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)
   } else if (postsStatus === 'failed') {
     content = <div>{postsError}</div>
   }
